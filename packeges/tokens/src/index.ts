598aa4270c75de6995713cbfa50da51fc4135f04
Pietro Weg <pietro.sera@compasso.com.br>
2022-12-06 21:38:42 -0300

    :lipstick: feat: configurando tokens

--- packeges/tokens/src/index.ts
+++ packeges/tokens/src/index.ts
@@ -2,3 +2,13 @@
 // import { colors } from '@ignite-ui/tokens/colors
 // import { colors } from '@ignite-ui/tokens
 export * from './colors'
+
+export * from './space'
+export * from './radii'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
+
+
