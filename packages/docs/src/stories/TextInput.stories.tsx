5dddb44a56ca79643ae7d69dbac2278e52483fc4
Pietro Weg <pietro.sera@compasso.com.br>
2022-12-27 23:22:15 -0300

    :book: docs: storybook textinput

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,40 @@
+import type { StoryObj, Meta } from '@storybook/react' // tipagens ts
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
+
+// Configuracao global do componente, será herdado em todas as variações
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">E-mail address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ], // customizar view que mostra nosso componente, colocar outros elementos
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
