4d0e4980344c6bd5bc4966d606070034137c91c9
Pietro Weg <pietro.sera@compasso.com.br>
2022-12-28 15:35:53 -0300

    :books: docs: storybook multistep

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,34 @@
+import type { StoryObj, Meta } from '@storybook/react' // tipagens ts
+import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+
+// Configuracao global do componente, será herdado em todas as variações
+export default {
+  title: 'Form/Multi Step',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {
+  args: {},
+}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,25 @@
+// import { ComponentProps } from 'react'
+import { Label, MultiStepContainer, Steps, Step } from './styles'
+
+export interface MultiStepProps {
+  size: number
+  currentStep?: number
+}
+
+export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
+  return (
+    <MultiStepContainer>
+      <Label>
+        Passo {currentStep} de {size}
+      </Label>
+
+      <Steps css={{ '--steps-size': size }}>
+        {/* criando um array do zero */}
+        {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from#using_arrow_functions_and_array.from */}
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => {
+          return <Step key={step} active={currentStep >= step} />
+        })}
+      </Steps>
+    </MultiStepContainer>
+  )
+}
