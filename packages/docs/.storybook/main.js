3360053f5a5fe7e8265142c1ff96154c5de4da9c
Pietro Weg <pietro.sera@compasso.com.br>
2023-01-03 14:07:32 -0300

    fix: add subpath to storybook on prod

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,13 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-call-design-system/'
+    }
+
+
+    return config
   }
 }
